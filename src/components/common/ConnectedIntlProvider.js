60f746136d1e706a57dc80e7dd2b6ec8c341db60
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-08-21 12:17:12 +0300

    Get rid of commented unused code in connected intl provider component

--- src/components/common/ConnectedIntlProvider.js
+++ src/components/common/ConnectedIntlProvider.js
@@ -1,9 +1,6 @@
 import { connect } from "react-redux";
 import { IntlProvider } from "react-intl";
 
-// const mapStateToProps = state => {
-//   lang: state.lang;
-// };
 function mapStateToProps(state) {
   const { lang } = state;
   return { locale: lang.locale, messages: lang.messages };
