8045182005aa566ab04e5fbf67b61150d67bd758
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-09-18 12:22:31 +0300

    Display commands table in tournament main component

--- src/components/tournaments/TournamentMain.js
+++ src/components/tournaments/TournamentMain.js
@@ -11,78 +11,79 @@ import {
   Dropdown,
   NavItem,
   Button,
-  Input
+  Input,
+  CardPanel
 } from "react-materialize";
 
 class TournamentMain extends Component {
   render() {
+    const { tournament } = this.props.tournaments;
+    let commandsList;
+
+    if (tournament) {
+      commandsList = tournament.commands.map((command, i) => (
+        <tr key={command.command_id}>
+          <td>{i + 1}</td>
+          <td className="tournament-command-row">
+            <img
+              src={command.logo}
+              alt=""
+              style={{ height: 25, marginRight: 8 }}
+            />
+            <span>{command.title}</span>
+          </td>
+          <td />
+          <td>{command.disgoals}</td>
+          <td>{command.pts}</td>
+        </tr>
+      ));
+    }
+
     return (
       <section className="section-tournament-main">
         <div className="container">
           <Row>
             <Col m={6}>
-              <table className="responsive-table">
+              <table className="responsive-table  z-depth-2">
                 <thead>
                   <tr className="tournament-table-head">
-                    <th>Таблица</th>
+                    <th colSpan={5}>Таблица</th>
+                  </tr>
+                  <tr>
+                    <th>Позиция</th>
+                    <th>Клуб</th>
+                    <th>И</th>
+                    <th>РГ</th>
+                    <th>О</th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr>
-                    <td>Alvin</td>
-                    <td>Eclair</td>
-                    <td>$0.87</td>
-                  </tr>
-                  <tr>
-                    <td>Alan</td>
-                    <td>Jellybean</td>
-                    <td>$3.76</td>
-                  </tr>
-                  <tr>
-                    <td>Jonathan</td>
-                    <td>Lollipop</td>
-                    <td>$7.00</td>
-                  </tr>
-                  <tr>
-                    <td>Shannon</td>
-                    <td>KitKat</td>
-                    <td>$9.99</td>
-                  </tr>
-                </tbody>
+                <tbody>{commandsList}</tbody>
               </table>
             </Col>
             <Col m={6}>
-              <table class="responsive-table">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Item Name</th>
-                    <th>Item Price</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Alvin</td>
-                    <td>Eclair</td>
-                    <td>$0.87</td>
-                  </tr>
-                  <tr>
-                    <td>Alan</td>
-                    <td>Jellybean</td>
-                    <td>$3.76</td>
-                  </tr>
-                  <tr>
-                    <td>Jonathan</td>
-                    <td>Lollipop</td>
-                    <td>$7.00</td>
-                  </tr>
-                  <tr>
-                    <td>Shannon</td>
-                    <td>KitKat</td>
-                    <td>$9.99</td>
-                  </tr>
-                </tbody>
-              </table>
+              <div>
+                <div className="tournament-table-head">
+                  <h3>Топ</h3>
+                </div>
+                <CardPanel>
+                  <Tabs className="tournament-table-tabs">
+                    <Tab title="Последние матчи" active>
+                      <div className="tournament-slider-wrap">
+                        {/* <Slider {...settings} {...this.props}>
+                          {lastGamesList}
+                        </Slider> */}
+                      </div>
+                    </Tab>
+                    <Tab title="Ближайшие матчи">
+                      <div className="tournament-slider-wrap">
+                        {/* <Slider {...settings} {...this.props}>
+                          {nextGamesList}
+                        </Slider> */}
+                      </div>
+                    </Tab>
+                  </Tabs>
+                </CardPanel>
+              </div>
             </Col>
           </Row>
         </div>
