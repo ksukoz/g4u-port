39e4b51bafbb01834d11e21dcd168a3c0bd7f0b2
ksukoz <seniurbano@gmail.com>
2018-09-26 14:19:16 +0300

    Add tables to tournament contacts component

--- src/components/tournaments/TournamentContacts.js
+++ src/components/tournaments/TournamentContacts.js
@@ -1,23 +1,71 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getTourContacts } from "../../actions/tournamentActions";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getTourContacts } from '../../actions/tournamentActions';
+
+import { Row, Col, Input, Button, Collection, CollectionItem, CardPanel } from 'react-materialize';
+import MatchesTable from '../common/MatchesTable';
+import MainTable from '../common/MainTable';
 
 class TournamentContacts extends Component {
-  componentDidMount = () => {
-    this.props.getTourContacts(this.props.id);
-  };
+	componentDidMount = () => {
+		this.props.getTourContacts(this.props.id);
+	};
 
-  render() {
-    return <div>TournamentContacts</div>;
-  }
+	render() {
+		const { contacts } = this.props.tournaments;
+
+		return (
+			<section className="section-tournament-main">
+				<div className="container">
+					<Row>
+						<Col s={12} l={8}>
+							{/* <Row>{newsList}</Row> */}
+						</Col>
+
+						<Col s={12} l={4}>
+							<MainTable
+								l={12}
+								title="Таблица"
+								commands={
+									this.props.tournaments && this.props.tournaments.contacts ? (
+										this.props.tournaments.contacts.table
+									) : (
+										[]
+									)
+								}
+							/>
+							<MatchesTable
+								l={12}
+								title="Последние матчи"
+								games={
+									this.props.tournaments && this.props.tournaments.contacts ? (
+										this.props.tournaments.contacts.lastgames
+									) : (
+										[]
+									)
+								}
+							/>
+							<MatchesTable
+								l={12}
+								title="Ближайшие матчи"
+								games={
+									this.props.tournaments && this.props.tournaments.contacts ? (
+										this.props.tournaments.contacts.begingames
+									) : (
+										[]
+									)
+								}
+							/>
+						</Col>
+					</Row>
+				</div>
+			</section>
+		);
+	}
 }
 
-const mapStateToProps = state => ({
-  tournaments: state.tournaments,
-  leagues: state.leagues
+const mapStateToProps = (state) => ({
+	tournaments: state.tournaments
 });
 
-export default connect(
-  mapStateToProps,
-  { getTourContacts }
-)(TournamentContacts);
+export default connect(mapStateToProps, { getTourContacts })(TournamentContacts);
