72bdf1443586ea8054fdd446a8f48f1324f2915c
ksukoz <seniurbano@gmail.com>
2018-10-05 12:05:21 +0300

    Fixing events graph component

--- src/components/common/EventsGraph.js
+++ src/components/common/EventsGraph.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React from 'react';
 
-const EventsGraph = props => {
-  const arr = [];
-  for (let i = 10; i < props.end - (props.end % 10) - 10; i = i + 10) {
-    arr.push(i);
-  }
+const EventsGraph = (props) => {
+	const arr = [];
+	for (let i = 10; i < props.end - props.end % 10 - 10; i = i + 10) {
+		arr.push(i);
+	}
 
-  return (
-    <div className="events-graph">
-      <div className="events-graph-bg">
-        <div className="events-periods">
-          <span>0</span>
-          {arr.map(item => (
-            <span>{item}</span>
-          ))}
-          <span>{props.end}</span>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="events-graph">
+			<div className="events-graph-bg">
+				<div className="events-periods">
+					<span>0</span>
+					{arr.map((item) => <span>{item}</span>)}
+					<span>{props.end}</span>
+				</div>
+				<div className="events-wrap">
+					{props.events.map((event) => (
+						<div
+							className={`events-item ${event.type === 'up' ? 'up' : 'down'}`}
+							style={{ left: `calc(${+event.minute / props.end * 100}% - 15px)` }}
+						>
+							<img src={event.icon} alt="" />
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default EventsGraph;
--- src/components/tournaments/GameComposition.js
+++ src/components/tournaments/GameComposition.js
@@ -1,107 +1,85 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { FormattedMessage } from "react-intl";
-import { getTourGameComposition } from "../../actions/tournamentActions";
-import {
-  Row,
-  Col,
-  Tabs,
-  Tab,
-  Dropdown,
-  NavItem,
-  Navbar,
-  Button,
-  Input,
-  Collection
-} from "react-materialize";
-import CollectionItem from "react-materialize/lib/CollectionItem";
-import EventsGraph from "../common/EventsGraph";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import { getTourGameComposition } from '../../actions/tournamentActions';
+import { Row, Col, Tabs, Tab, Dropdown, NavItem, Navbar, Button, Input, Collection } from 'react-materialize';
+import CollectionItem from 'react-materialize/lib/CollectionItem';
+import EventsGraph from '../common/EventsGraph';
 
 class GameComposition extends Component {
-  componentDidMount = () => {
-    this.props.getTourGameComposition(this.props.id);
-  };
+	componentDidMount = () => {
+		this.props.getTourGameComposition(this.props.id);
+	};
 
-  render() {
-    const { composition } = this.props.tournaments;
+	render() {
+		const { composition } = this.props.tournaments;
 
-    return (
-      <div className="game-composition">
-        <Row>
-          <Col s={12}>
-            <EventsGraph
-              end={
-                this.props.tournaments.composition
-                  ? this.props.tournaments.composition.maxmin
-                  : 60
-              }
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col m={4}>
-            {composition ? (
-              // console.log(composition.in)
-              <Collection className="z-depth-1">
-                {composition.in.map(item => (
-                  // <ListItem img={item.photo} key={item.plId} />
-                  <CollectionItem key={item.plid}>
-                    <img src={item.photo} alt="" />
-                    <div>
-                      <h3>{item.name}</h3>
-                      <p>{item.type}</p>
-                    </div>
-                  </CollectionItem>
-                ))}
-              </Collection>
-            ) : (
-              ""
-            )}
-          </Col>
-          <Col m={4}>
-            {composition ? (
-              <img
-                className="game-composition-field"
-                src={composition.field}
-                alt=""
-              />
-            ) : (
-              ""
-            )}
-          </Col>
-          <Col m={4}>
-            {composition ? (
-              // console.log(composition.in)
-              <Collection>
-                {composition.out.map(item => (
-                  // <ListItem img={item.photo} key={item.plId} />
-                  <CollectionItem key={item.plid}>
-                    <img src={item.photo} alt="" />
-                    <div>
-                      <h3>{item.name}</h3>
-                      <p>{item.type}</p>
-                    </div>
-                  </CollectionItem>
-                ))}
-              </Collection>
-            ) : (
-              ""
-            )}
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+		return (
+			<div className="game-composition">
+				<Row>
+					<Col s={12}>
+						<EventsGraph
+							end={this.props.tournaments.composition ? this.props.tournaments.composition.maxmin : 60}
+							events={this.props.tournaments.composition ? this.props.tournaments.composition.events : []}
+						/>
+					</Col>
+				</Row>
+				<Row>
+					<Col m={4}>
+						{composition ? (
+							// console.log(composition.in)
+							<Collection className="z-depth-1">
+								{composition.in.map((item) => (
+									// <ListItem img={item.photo} key={item.plId} />
+									<CollectionItem key={item.plid}>
+										<img src={item.photo} alt="" />
+										<div>
+											<h3>{item.name}</h3>
+											<p>{item.type}</p>
+										</div>
+									</CollectionItem>
+								))}
+							</Collection>
+						) : (
+							''
+						)}
+					</Col>
+					<Col m={4}>
+						{composition ? <img className="game-composition-field" src={composition.field} alt="" /> : ''}
+					</Col>
+					<Col m={4}>
+						{composition ? (
+							// console.log(composition.in)
+							<Collection>
+								{composition.out.map((item) => (
+									// <ListItem img={item.photo} key={item.plId} />
+									<CollectionItem key={item.plid}>
+										<img src={item.photo} alt="" />
+										<div>
+											<h3>{item.name}</h3>
+											<p>{item.type}</p>
+										</div>
+									</CollectionItem>
+								))}
+							</Collection>
+						) : (
+							''
+						)}
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => ({
-  tournaments: state.tournaments
+const mapStateToProps = (state) => ({
+	tournaments: state.tournaments
 });
 
 export default connect(
-  mapStateToProps,
-  // null,
-  { getTourGameComposition }
+	mapStateToProps,
+	// null,
+	{ getTourGameComposition }
 )(GameComposition);
