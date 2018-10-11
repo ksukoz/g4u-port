fe91bd6f1aef86507ffc4546e9b23552a597ba0d
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-10-11 12:46:49 +0300

    Fix tournament stadiums, tournament results and tournament clubs components

--- src/components/tournaments/TournamentResults.js
+++ src/components/tournaments/TournamentResults.js
@@ -1,72 +1,88 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getTourResults, getFilteredResults } from '../../actions/tournamentActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  getTourResults,
+  getFilteredResults
+} from "../../actions/tournamentActions";
 
-import { Row, Col, Input, Button, Collection, CollectionItem, CardPanel } from 'react-materialize';
-import ResultsTable from '../common/ResultsTable';
+import {
+  Row,
+  Col,
+  Input,
+  Button,
+  Collection,
+  CollectionItem,
+  CardPanel
+} from "react-materialize";
+import ResultsTable from "../common/ResultsTable";
+import TournamentHeader from "./TournamentHeader";
 
 class TournamentResults extends Component {
-	state = {
-		tour: 0,
-		club: 0,
-		stadium: 0
-	};
+  state = {
+    tour: 0,
+    club: 0,
+    stadium: 0
+  };
 
-	onChangeHandler = (e) => {
-		this.setState({
-			...this.state,
-			[e.target.name]: e.target.value
-		});
-		this.props.getFilteredResults(
-			this.props.id,
-			this.state.tour !== 0 ? `tour=${this.state.tour}` : '',
-			this.state.club !== 0 ? `comId=${this.state.club}` : '',
-			this.state.stadium !== 0 ? `stadId=${this.state.stadium}` : ''
-		);
-	};
+  onChangeHandler = e => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    });
+    this.props.getFilteredResults(
+      this.props.match.params.id,
+      this.state.tour !== 0 ? `tour=${this.state.tour}` : "",
+      this.state.club !== 0 ? `comId=${this.state.club}` : "",
+      this.state.stadium !== 0 ? `stadId=${this.state.stadium}` : ""
+    );
+  };
 
-	onClearClickHadler = () => {
-		this.setState({
-			tour: 0,
-			club: 0,
-			stadium: 0
-		});
+  onClearClickHadler = () => {
+    this.setState({
+      tour: 0,
+      club: 0,
+      stadium: 0
+    });
 
-		this.props.getTourResults(this.props.id);
-	};
+    this.props.getTourResults(this.props.match.params.id);
+  };
 
-	componentDidMount = () => {
-		this.props.getTourResults(this.props.id);
-	};
+  componentDidMount = () => {
+    this.props.getTourResults(this.props.match.params.id);
+  };
 
-	render() {
-		const { results } = this.props.tournaments;
+  render() {
+    const { results } = this.props.tournaments;
 
-		return (
-			<section className="section-tournament-main">
-				<div className="container">
-					<Row>
-						<ResultsTable
-							l={12}
-							stateTour={this.state.tour}
-							onChangeHandler={this.onChangeHandler}
-							stateClub={this.state.club}
-							stateStadium={this.state.stadium}
-							tour={results !== null ? results.result.filter.tour : []}
-							clubs={results !== null ? results.result.filter.commands : []}
-							stadiums={results !== null ? results.result.filter.statiums : []}
-							onClearClickHadler={this.onClearClickHadler}
-							gamelist={results !== null ? results.result.gamelist : []}
-						/>
-					</Row>
-				</div>
-			</section>
-		);
-	}
+    return (
+      <section className="section-tournament-main">
+        <TournamentHeader id={this.props.match.params.id} />
+        <div className="container">
+          <Row>
+            <ResultsTable
+              l={12}
+              stateTour={this.state.tour}
+              onChangeHandler={this.onChangeHandler}
+              stateClub={this.state.club}
+              stateStadium={this.state.stadium}
+              tour={results !== null ? results.result.filter.tour : []}
+              clubs={results !== null ? results.result.filter.commands : []}
+              stadiums={results !== null ? results.result.filter.statiums : []}
+              onClearClickHadler={this.onClearClickHadler}
+              gamelist={results !== null ? results.result.gamelist : []}
+            />
+          </Row>
+        </div>
+      </section>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-	tournaments: state.tournaments
+const mapStateToProps = state => ({
+  tournaments: state.tournaments
 });
 
-export default connect(mapStateToProps, { getTourResults, getFilteredResults })(TournamentResults);
+export default connect(
+  mapStateToProps,
+  { getTourResults, getFilteredResults }
+)(TournamentResults);
--- src/components/tournaments/TournamentsStadiums.js
+++ src/components/tournaments/TournamentsStadiums.js
@@ -1,124 +1,154 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getTourStadiums } from '../../actions/tournamentActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getTourStadiums } from "../../actions/tournamentActions";
 
-import { compose, withProps, lifecycle } from 'recompose';
+import { compose, withProps, lifecycle } from "recompose";
 
-import { Row, Col, Input, Button, Collection, CollectionItem, CardPanel } from 'react-materialize';
-import MatchesTable from '../common/MatchesTable';
-import MainTable from '../common/MainTable';
+import {
+  Row,
+  Col,
+  Input,
+  Button,
+  Collection,
+  CollectionItem,
+  CardPanel
+} from "react-materialize";
+import MatchesTable from "../common/MatchesTable";
+import MainTable from "../common/MainTable";
 
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoBox } from 'react-google-maps';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoBox
+} from "react-google-maps";
+import TournamentHeader from "./TournamentHeader";
 
 const MyMapComponent = compose(
-	withProps({
-		googleMapURL:
-			'https://maps.googleapis.com/maps/api/js?key=AIzaSyAwlzhR2g7O7r4r4pwVUz-Hc60Oz4T3GqY&language=en&libraries=geometry,drawing,places',
-		loadingElement: <div style={{ height: `400px` }} />,
-		containerElement: <div style={{ height: `400px` }} />,
-		mapElement: <div style={{ height: `100%`, borderBottomLeftRadius: 20, borderBottomRightRadius: 20 }} />
-	}),
-	withScriptjs,
-	withGoogleMap,
-	lifecycle({
-		componentDidUpdate() {}
-	})
-)((props) => (
-	<GoogleMap defaultZoom={17} defaultCenter={props.defaultCenter} onClick={props.onClick}>
-		{props.isMarkerShown && <Marker position={{ lat: props.lat, lng: props.lng }} onClick={props.onToggleOpen} />}
-	</GoogleMap>
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAwlzhR2g7O7r4r4pwVUz-Hc60Oz4T3GqY&language=en&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: (
+      <div
+        style={{
+          height: `100%`,
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20
+        }}
+      />
+    )
+  }),
+  withScriptjs,
+  withGoogleMap,
+  lifecycle({
+    componentDidUpdate() {}
+  })
+)(props => (
+  <GoogleMap
+    defaultZoom={17}
+    defaultCenter={props.defaultCenter}
+    onClick={props.onClick}
+  >
+    {props.isMarkerShown && (
+      <Marker
+        position={{ lat: props.lat, lng: props.lng }}
+        onClick={props.onToggleOpen}
+      />
+    )}
+  </GoogleMap>
 ));
 
 class TournamentsStadiums extends Component {
-	componentDidMount = () => {
-		this.props.getTourStadiums(this.props.id);
-	};
+  componentDidMount = () => {
+    this.props.getTourStadiums(this.props.match.params.id);
+  };
 
-	render() {
-		const { stadiums } = this.props.tournaments;
+  render() {
+    const { stadiums } = this.props.tournaments;
 
-		let stadiumsList;
+    let stadiumsList;
 
-		if (stadiums !== null) {
-			stadiumsList = stadiums.stadiums.map(
-				(stadium) =>
-					stadium.stId !== '0' ? (
-						<Col s={12} key={stadium.stId}>
-							<div className="z-depth-2 stadium-card">
-								<div className="stadium-header">
-									<h4>{stadium.stitle}</h4>
-									<address>{stadium.address}</address>
-								</div>
-								<MyMapComponent
-									isMarkerShown
-									defaultCenter={{
-										lat: parseFloat(stadium.latitude),
-										lng: parseFloat(stadium.longitude)
-									}}
-									googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAwlzhR2g7O7r4r4pwVUz-Hc60Oz4T3GqY&language=en&libraries=geometry,drawing,places"
-									lat={parseFloat(stadium.latitude)}
-									lng={parseFloat(stadium.longitude)}
-								/>
-							</div>
-						</Col>
-					) : (
-						''
-					)
-			);
-		}
+    if (stadiums !== null) {
+      stadiumsList = stadiums.stadiums.map(
+        stadium =>
+          stadium.stId !== "0" ? (
+            <Col s={12} key={stadium.stId}>
+              <div className="z-depth-2 stadium-card">
+                <div className="stadium-header">
+                  <h4>{stadium.stitle}</h4>
+                  <address>{stadium.address}</address>
+                </div>
+                <MyMapComponent
+                  isMarkerShown
+                  defaultCenter={{
+                    lat: parseFloat(stadium.latitude),
+                    lng: parseFloat(stadium.longitude)
+                  }}
+                  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAwlzhR2g7O7r4r4pwVUz-Hc60Oz4T3GqY&language=en&libraries=geometry,drawing,places"
+                  lat={parseFloat(stadium.latitude)}
+                  lng={parseFloat(stadium.longitude)}
+                />
+              </div>
+            </Col>
+          ) : (
+            ""
+          )
+      );
+    }
 
-		return (
-			<section className="section-tournament-main">
-				<div className="container">
-					<Row>
-						<Col s={12} l={8}>
-							{stadiumsList}
-						</Col>
+    return (
+      <section className="section-tournament-main">
+        <TournamentHeader id={this.props.match.params.id} />
+        <div className="container">
+          <Row>
+            <Col s={12} l={8}>
+              {stadiumsList}
+            </Col>
 
-						<Col s={12} l={4}>
-							<MainTable
-								l={12}
-								title="Таблица"
-								commands={
-									this.props.tournaments && this.props.tournaments.stadiums ? (
-										this.props.tournaments.stadiums.table
-									) : (
-										[]
-									)
-								}
-							/>
-							<MatchesTable
-								l={12}
-								title="Последние матчи"
-								games={
-									this.props.tournaments && this.props.tournaments.stadiums ? (
-										this.props.tournaments.stadiums.lastgames
-									) : (
-										[]
-									)
-								}
-							/>
-							<MatchesTable
-								l={12}
-								title="Ближайшие матчи"
-								games={
-									this.props.tournaments && this.props.tournaments.stadiums ? (
-										this.props.tournaments.stadiums.begingames
-									) : (
-										[]
-									)
-								}
-							/>
-						</Col>
-					</Row>
-				</div>
-			</section>
-		);
-	}
+            <Col s={12} l={4}>
+              <MainTable
+                l={12}
+                title="Таблица"
+                commands={
+                  this.props.tournaments && this.props.tournaments.stadiums
+                    ? this.props.tournaments.stadiums.table
+                    : []
+                }
+              />
+              <MatchesTable
+                l={12}
+                title="Последние матчи"
+                games={
+                  this.props.tournaments && this.props.tournaments.stadiums
+                    ? this.props.tournaments.stadiums.lastgames
+                    : []
+                }
+              />
+              <MatchesTable
+                l={12}
+                title="Ближайшие матчи"
+                games={
+                  this.props.tournaments && this.props.tournaments.stadiums
+                    ? this.props.tournaments.stadiums.begingames
+                    : []
+                }
+              />
+            </Col>
+          </Row>
+        </div>
+      </section>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-	tournaments: state.tournaments
+const mapStateToProps = state => ({
+  tournaments: state.tournaments
 });
 
-export default connect(mapStateToProps, { getTourStadiums })(TournamentsStadiums);
+export default connect(
+  mapStateToProps,
+  { getTourStadiums }
+)(TournamentsStadiums);
